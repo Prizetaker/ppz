d4fa937dea29a00575f10f6abb294d38a358a8f3
Prizetaker <jobmailpost@gmail.com>
2017-01-28 00:02:15 +0200

    Create Additional.java

---
+++ Additional.java
@@ -0,0 +1,13 @@
+package dnp.Fourth.Homework;
+
+import java.io.IOException;
+
+
+public interface Additional {
+    void addSt() throws Except, IOException;
+    void delStud();
+    void findStud();
+    void sortSurname();
+    
+    
+}
