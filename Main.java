25d81128526f8d2c6e95ab63914b369d6b7c7d0a
Prizetaker <jobmailpost@gmail.com>
2017-02-03 00:52:21 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package dnp.Fifth.Homework;
+
+public class Main {
+	public static void main(String[] args) {
+		Filecopying fc = new Filecopying("D:\\My Documents\\2\\", "D:\\My Documents\\3\\");
+		fc.copyTextToNewFiles();
+
+	}
+}
